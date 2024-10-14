e89900aa1967fe149219df997b5c1fa50c903706
Luis Mu <luismu19@gmail.com>
2024-10-14 10:38:00 -0500

    fixing and updating library

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import Tilt from "react-tilt";
+import {Tilt} from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
+import { defaultOptions } from "../utils/tiltDefaultoptions";
+
+
 
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <Tilt options={defaultOptions} className='xs:w-[250px] w-full'>
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
